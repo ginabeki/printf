41045af410eb4543bd61ae6d5402bb06d7d9c362
chrisokwisa <okwisachristabel@gmail.com>
2022-04-19 13:21:41 +0000

    commit

--- modifiers.c
+++ modifiers.c
@@ -10,6 +10,7 @@ unsigned int print_neg_width(buffer_t *output, unsigned int printed,
 /**
  * print_width - Stores leading spaces to a buffer for a width modifier.
  * @output: A buffer_t struct containing a character array.
+ *
  * @printed: The current number of characters already printed to output
  *           for a given number specifier.
  * @flags: Flag modifiers.
