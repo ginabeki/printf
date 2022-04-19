22b548a753d1f1a86c6531b2d75c208b963053d1
chrisokwisa <99495858+chrisokwisa@users.noreply.github.com>
2022-04-19 09:36:36 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,34 +1,46 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef PRINT_F
+#define PRINT_F
 
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <limits.h>
 
 /**
-* struct op - flag / function object
-* @c: flag
-* @f: function
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
 */
-
-typedef struct op
+struct convert
 {
-	char *c;
+	char *sym;
 	int (*f)(va_list);
-} op_t;
+};
+typedef struct convert conver_t;
 
+/*Main functions*/
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
-int _putchar(char c);
-int print_c(va_list ar_list);
-int (*get_func(char s))(va_list ar_list);
-int print_s(va_list ar_list);
-int print_d(va_list ar_list);
-int print_i(va_list ar_numlist);
-int print_b(va_list binary_list);
-int print_u(va_list ar_list);
-int print_o(va_list ar_list);
-int print_x(va_list ar_list);
-int print_X(va_list ar_list);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+/*Helper functions*/
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
+
 
 #endif
